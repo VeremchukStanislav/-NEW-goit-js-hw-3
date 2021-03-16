080fb82881f727e0467b6ab83bc74592b8a35826
VeremchukStanislav <verenchuk.stanislav@gmail.com>
2021-03-16 17:18:02 +0200

    done task-11

---
+++ JS/task-11.js
@@ -0,0 +1,24 @@
+// Выполни рефакторинг решения предыдущего задания добавив в цикл for...in проверку на собственное свойство.
+
+const keys = [];
+const values = [];
+const advert = {
+  service: 'apt',
+};
+const apartment = Object.create(advert);
+apartment.descr = 'Просторная квартира в центре';
+apartment.rating = 4;
+apartment.price = 2153;
+
+for (const key in apartment) {
+  // Пиши код ниже этой строки
+    if (apartment.hasOwnProperty(key)) {
+          keys.push(key);
+  values.push(apartment[key]);
+
+}
+
+  // Пиши код выше этой строки
+}
+console.log(keys);
+console.log(values);
